96022c6d699138c6757c0633d7b92b62bb212152
Foxscotch <foxscotch@gmail.com>
2015-12-14 19:32:04 -0500

    Finished day 1 part 1

---
+++ day-1-1.py
@@ -0,0 +1,19 @@
+import requests
+
+
+with open('day-1-input.txt', 'r') as f:
+    puzzle_input = f.read()
+
+
+current_floor = 0
+
+for paren in puzzle_input:
+    if paren == '(':
+        current_floor += 1
+    elif paren == ')':
+        current_floor -= 1
+
+print(current_floor)
+
+
+# My answer: 74
