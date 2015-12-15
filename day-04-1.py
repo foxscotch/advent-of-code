7d04929f83b378e0c260c334dd993ee99dde4e25
damienmh <foxscotch@gmail.com>
2015-12-15 02:29:14 -0500

    Complete day 4 part 1

--- day-04-1.py
+++ day-04-1.py
@@ -1,4 +1,18 @@
 import hashlib
 
 
-puzzle_input = 'iwrupvqb'
+puzzle_input = b'iwrupvqb'
+number = 100000
+
+while True:
+    key = puzzle_input + str(number).encode()
+    if hashlib.md5(key).hexdigest()[:5] == '00000':
+        break
+    number += 1
+
+print(number)
+
+# Runs way faster than I expected, lol
+
+
+# My answer: 346386
