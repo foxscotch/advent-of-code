5ddf1a53f95139c0b2679a5f368d65626c23a9ee
Foxscotch <foxscotch@gmail.com>
2016-10-27 12:52:11 -0500

    Finish 2015 day 7 part 2

---
+++ 2015/day-07/p2.py
@@ -0,0 +1,123 @@
+# god, this is so overkill
+
+class Board:
+    def __init__(self, file):
+        self.instructions = []
+        self.wires = WireManager()
+
+        for line in file:
+            tokens = [int(i) if i.isdigit() else i.strip() for i in line.split(' ')]
+            self.instructions.append(Instruction(self, tokens))
+
+        for instr in self.instructions:
+            for op in instr.operands:
+                if type(op) is str:
+                    self.wires.add_wire(op)
+            self.wires.add_wire(instr.dest)
+
+    def run(self):
+        skipped = False
+
+        for instr in self.instructions:
+            if instr.is_runnable():
+                instr.run()
+            else:
+                skipped = True
+
+        if skipped:
+            self.run()
+
+
+class WireManager:
+    def __init__(self):
+        self._wires = {}
+
+    def add_wire(self, wire):
+        wire = Wire(wire)
+        self._wires[wire.name] = wire
+
+    def get_wire(self, wire):
+        return self._wires[wire].value
+
+    def set_wire(self, wire, value):
+        if type(value) is str:
+            value = self.get_wire(value)
+        self._wires[wire].value = value
+
+    def reset(self, exceptions=None):
+        if exceptions is None:
+            exceptions = []
+        for wire in self._wires.values():
+            if wire.name not in exceptions:
+                wire.value = None
+
+    def check_runnable(self, *wires):
+        for wire in wires:
+            if type(wire) is not int and self.get_wire(wire) is None:
+                return False
+        return True
+
+    def __str__(self):
+        return '\n'.join([str(wire) for wire in self._wires.values()])
+
+
+class Wire:
+    def __init__(self, name):
+        self.name = name
+        self.value = None
+
+    def __str__(self):
+        return '{}={}'.format(self.name, self.value)
+
+
+class Instruction:
+    def __init__(self, board, tokens):
+        self.board = board
+        self.dest = tokens[-1]
+
+        if len(tokens) == 4:
+            self.operator = 'NOT'
+            self.operands = [tokens[1]]
+        elif len(tokens) == 5:
+            self.operator = tokens[1]
+            self.operands = [tokens[0], tokens[2]]
+        else:
+            self.operator = 'ASSIGN'
+            if self.dest is 'b':
+                self.operands = [3176]
+            else:
+                self.operands = [tokens[0]]
+
+    def is_runnable(self):
+        return self.board.wires.check_runnable(*self.operands)
+
+    def run(self):
+        operands = []
+
+        for op in self.operands:
+            if type(op) != int:
+                operands.append(self.board.wires.get_wire(op))
+            else:
+                operands.append(op)
+
+        if self.operator == 'NOT':
+            self.board.wires.set_wire(self.dest, ~operands[0])
+        elif self.operator == 'LSHIFT':
+            self.board.wires.set_wire(self.dest, operands[0] << operands[1])
+        elif self.operator == 'RSHIFT':
+            self.board.wires.set_wire(self.dest, operands[0] >> operands[1])
+        elif self.operator == 'AND':
+            self.board.wires.set_wire(self.dest, operands[0] & operands[1])
+        elif self.operator == 'OR':
+            self.board.wires.set_wire(self.dest, operands[0] | operands[1])
+        else:
+            # ASSIGN
+            self.board.wires.set_wire(self.dest, operands[0])
+
+
+with open('input.txt', 'r') as file:
+    board = Board(file)
+
+
+board.run()
+print(board.wires.get_wire('a'))
