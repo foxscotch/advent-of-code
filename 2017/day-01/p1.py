990954352e98ad710593d23494bdc6abc3e89b00
Foxscotch <foxscotch@gmail.com>
2017-12-01 13:02:07 -0600

    Improve names in 2017.01

--- 2017/day-01/p1.py
+++ 2017/day-01/p1.py
@@ -1,15 +1,15 @@
 # Python 3.6.1 / Anaconda 4.4.0
 
 with open('input.txt', 'r') as f:
-    puz_in = [int(i) for i in f.read()[0:-1]]
+    puzzle_input = [int(i) for i in f.read()[0:-1]]
 
-puz_sum = 0
-for pc in range(len(puz_in)):
-    pn = pc + 1 if not pc == len(puz_in) - 1 else 0
-    puz_cur = puz_in[pc]
-    puz_next = puz_in[pn]
+total = 0
+for cur_index in range(len(puzzle_input)):
+    next_index = cur_index + 1 if not cur_index == len(puzzle_input) - 1 else 0
+    puz_cur = puzzle_input[cur_index]
+    pnext = puzzle_input[next_index]
 
-    if puz_cur == puz_next:
-        puz_sum += puz_cur
+    if puz_cur == pnext:
+        total += puz_cur
 
-print(puz_sum)
+print(total)
