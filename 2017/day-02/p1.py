af8a69107b2b2d8a488f115b92ed00cca181ae38
Foxscotch <foxscotch@gmail.com>
2017-12-02 23:56:51 -0500

    Fix tabs in 2017.02.1

--- 2017/day-02/p1.py
+++ 2017/day-02/p1.py
@@ -6,10 +6,10 @@ import re
 with open('input.txt', 'r') as f:
     puzzle_input = []
     for line in f:
-      puzzle_input.append([int(i) for i in re.split('\s', line)[:-1]])
+        puzzle_input.append([int(i) for i in re.split('\s', line)[:-1]])
 
 total = 0
 for row in puzzle_input:
-  total += max(row) - min(row)
+    total += max(row) - min(row)
 
 print(total)
