c702cd5b88c9dd2c802612dcba517aba9407692a
Foxscotch <foxscotch@gmail.com>
2017-12-03 00:02:34 -0500

    Complete 2017.02.2

---
+++ 2017/day-02/p2.py
@@ -0,0 +1,18 @@
+# Python 3.6.1 / Official
+
+import re
+
+
+with open('input.txt', 'r') as f:
+    puzzle_input = []
+    for line in f:
+        puzzle_input.append([int(i) for i in re.split('\s', line)[:-1]])
+
+total = 0
+for row in puzzle_input:
+    for dividend in row:
+        for divisor in row:
+            if dividend > divisor and dividend % divisor == 0:
+                total += dividend // divisor
+
+print(total)
