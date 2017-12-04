b3550f44a4106686124e6eca8ef653d0f6a47dd8
Foxscotch <foxscotch@gmail.com>
2017-12-04 17:05:20 -0600

    Complete 2017.04.1

---
+++ 2017/day-04/p1.py
@@ -0,0 +1,18 @@
+# Python 3.6.1 / Official
+
+with open('input.txt', 'r') as f:
+    puzzle_input = []
+    for line in f:
+        puzzle_input.append(line.strip().split(' '))
+
+total = 0
+for phrase in puzzle_input:
+    bad = False
+    for word in phrase:
+        if phrase.count(word) > 1:
+            bad = True
+            break
+    if not bad:
+        total += 1
+
+print(total)
