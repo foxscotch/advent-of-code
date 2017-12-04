b0f60cae4896814d29e7c27a7cc1bde9e5e756eb
Foxscotch <foxscotch@gmail.com>
2017-12-04 16:38:51 -0600

    Complete 2017.03.1

--- 2017/day-03/p1.py
+++ 2017/day-03/p1.py
@@ -1,32 +1,25 @@
 # Python 3.6.1 / Official
 
-puzzle_input = 25 #265149
-
-def same_sign(a, b):
-    return abs(a + b) == abs(a) + abs(b)
+puzzle_input = 265149
 
 x = 0
 y = 0
 cur_num = 1
 cur_layer = 0
-while cur_num != puzzle_input:
-    to_print = []
 
+while cur_num != puzzle_input:
     if y == -cur_layer and x == cur_layer:
         x += 1
         cur_layer += 1
-        to_print.append('NEW_LAYER {:>2}'.format(cur_layer))
-    elif abs(y) == cur_layer:
-        if y > 0 and x > -cur_layer:
-            x -= 1
-        elif y < 0 and x < cur_layer:
-            x += 1
-        else:
-            to_print.append('BROKE_ON_X_MOVE')
-    elif abs(x) == cur_layer:
-        pass
-    else:
-        to_print.append('BROKE_IN_GENERAL')
+    elif y == cur_layer and x > -cur_layer:
+        x -= 1
+    elif y == -cur_layer and x < cur_layer:
+        x += 1
+    elif x == cur_layer and y < cur_layer:
+        y += 1
+    elif x == -cur_layer and y > -cur_layer:
+        y -= 1
 
     cur_num += 1
-    print('NUMBER {:>2}    POS ({:> 3}, {:> 3})    {}'.format(cur_num, x, y, '    '.join(to_print)))
+
+print('RESULT {}'.format(abs(x) + abs(y)))
