d99a52c53f1a1dc5ba4b8e4130f3c52f44f32d9d
Foxscotch <foxscotch@gmail.com>
2017-12-05 09:04:58 -0600

    Complete 2017.05

---
+++ 2017/day-05/p1.py
@@ -0,0 +1,17 @@
+# Python 3.6.1 / Official
+
+with open('input.txt', 'r') as f:
+    puzzle_input = []
+    for line in f:
+        puzzle_input.append(int(line))
+
+i = 0
+steps = 0
+while True:
+    puzzle_input[i] += 1
+    i += puzzle_input[i] - 1
+    steps += 1
+    if i < 0 or i >= len(puzzle_input):
+        break
+
+print(steps)
