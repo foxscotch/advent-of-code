07012d886330f109fbc8c10d1d9ce5ecba8691aa
Foxscotch <foxscotch@gmail.com>
2017-12-12 11:29:08 -0600

    Complete 2017.09.2

---
+++ 2017/09/p2.py
@@ -0,0 +1,38 @@
+# Python 3.6.1
+# Requires: anytree 2.4.2
+
+from anytree import AnyNode as Node, PreOrderIter
+
+
+def get_input():
+    with open('input.txt', 'r') as f:
+        return f.read()
+
+def get_score(text):
+    score = 0
+    with open('input.txt', 'r') as f:
+        i = 0
+        garbage = False
+        while True:
+            if i == len(text):
+                break
+            char = text[i]
+
+            if not garbage and char == '<':
+                garbage = True
+            elif garbage and char == '>':
+                garbage = False
+            elif garbage and char == '!':
+                i += 1
+            elif garbage:
+                score += 1
+
+            i += 1
+    return score
+
+def main():
+    print(get_score(get_input()))
+
+
+if __name__ == '__main__':
+    main()
