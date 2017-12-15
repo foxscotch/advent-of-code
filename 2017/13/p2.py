3dd9719d7d9251506b5abb5c0a2542d62bc2c4a8
Foxscotch <foxscotch@gmail.com>
2017-12-15 15:11:40 -0600

    Complete 2017.13.2

---
+++ 2017/13/p2.py
@@ -0,0 +1,75 @@
+# Python 3.6.1
+
+import re
+import sys
+from pdb import set_trace
+
+
+"""
+This version is significantly faster than p2_slow.py. I don't think you could
+reasonably ever reach the answer for this using that one. Anyway, for this one,
+I took an approach where I _calculate_ the 'position' of the scanner in the
+current row. I think it's pretty neat. Probably could've thought about it
+sooner, and made p1 faster too, but oh well. Still, cool. Way faster. It does
+still take a while though. I might see if I can think of a way to speed it up
+even more, sometime.
+
+I just kept the slow version for posterity.
+"""
+
+
+class Firewall:
+    def __init__(self, layers):
+        self.pos = -1
+        self.penalty = 0
+        self.caught = False
+
+        self.layers = [None for i in range(layers[-1][0] + 1)]
+        for layer in layers:
+            self.layers[layer[0]] = (layer[1] - 1) * 2
+
+    def move(self, step=1):
+        for _ in range(len(self.layers)):
+            self.pos += 1
+            self.move_once(step)
+            step += 1
+
+    def move_once(self, step):
+            layer = self.layers[self.pos]
+            if layer is not None:
+                if step % layer == 1:
+                    self.penalty += self.pos * (layer // 2 + 1)
+                    self.caught = True
+
+    def reset(self):
+        self.pos = -1
+        self.penalty = 0
+        self.caught = False
+
+    def __repr__(self):
+        return f'Firewall({self.pos}, {self.penalty}, {self.caught})'
+
+
+def get_input():
+    r = re.compile(r'(\d{1,2}): (\d{1,2})').match
+    with open('input.txt', 'r') as f:
+        return [(int(r(l).group(1)), int(r(l).group(2))) for l in f]
+
+def main():
+    inp = get_input()
+    f = Firewall(inp)
+
+    delay = 0
+    while True:
+        f.move(delay + 1)
+        if f.caught:
+            delay += 1
+            f.reset()
+        else:
+            break
+
+    print(delay)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/13/p2_slow.py
@@ -0,0 +1,88 @@
+# Python 3.6.1
+
+import re
+from pdb import set_trace
+
+
+class Firewall:
+    def __init__(self, layers):
+        self.pos = -1
+        self.penalty = 0
+        self.caught = False
+
+        self.layers = [None for i in range(layers[-1][0] + 1)]
+        for layer in layers:
+            self.layers[layer[0]] = Layer(layer[1])
+
+    def move(self):
+        for _ in range(len(self.layers)):
+            self.move_once()
+
+    def move_with_delay(self, delay):
+        for _ in range(delay):
+            self.move_scanners()
+        self.move()
+
+    def move_once(self):
+        self.move_packet()
+
+        layer = self.layers[self.pos]
+        if type(layer) is Layer:
+            if layer.pos == 0:
+                self.penalty += self.pos * layer.range
+                self.caught = True
+
+        self.move_scanners()
+
+    def move_packet(self):
+        self.pos += 1
+
+    def move_scanners(self):
+        for layer in self.layers:
+            if layer is not None:
+                layer.move_scanner()
+
+class Layer:
+    def __init__(self, range):
+        self.range = range
+        self.pos = 0
+
+        self.moving_down = True
+
+    def move_scanner(self):
+        if self.moving_down:
+            self.pos += 1
+        else:
+            self.pos -= 1
+
+        if self.pos == 0 or self.pos == self.range - 1:
+            self.moving_down = not self.moving_down
+
+    def __repr__(self):
+        return f'Layer({len(self.list)})'
+
+
+def get_input():
+    r = re.compile(r'(\d{1,2}): (\d{1,2})').match
+    with open('input.txt', 'r') as f:
+        return [(int(r(l).group(1)), int(r(l).group(2))) for l in f]
+
+def main():
+    inp = get_input()
+
+    delay = 0
+    while True:
+        f = Firewall([(0, 3), (1, 2), (4, 4), (6, 4)])
+        f = Firewall(inp)
+        f.move_with_delay(delay)
+        print(delay)
+        if f.caught:
+            delay += 1
+        else:
+            break
+
+    print(delay)
+
+
+if __name__ == '__main__':
+    main()
