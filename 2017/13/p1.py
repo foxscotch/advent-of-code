717de79e07bc1d054105007e4d008e0cb372b775
Foxscotch <foxscotch@gmail.com>
2017-12-15 13:02:17 -0600

    Complete 2017.13.1

--- 2017/13/p1.py
+++ 2017/13/p1.py
@@ -1,31 +1,64 @@
 # Python 3.6.1
 
 import re
+from pdb import set_trace
 
 
 class Firewall:
     def __init__(self, layers):
-        super(Firewall, self).__init__()
-        self.pos = 0
+        self.pos = -1
+        self.penalty = 0
+
+        self.layers = [None for i in range(layers[-1][0] + 1)]
         for layer in layers:
+            self.layers[layer[0]] = Layer(layer[1])
+
+    def move(self):
+        for _ in range(len(self.layers)):
+            self.move_once()
+
+    def move_once(self):
+        self.move_packet()
+
+        layer = self.layers[self.pos]
+        if type(layer) is Layer:
+            if layer.pos == 0:
+                self.penalty += self.pos * len(layer.list)
+
+        self.move_scanners()
+
+    def move_packet(self):
+        self.pos += 1
+
+    def move_scanners(self):
+        for layer in self.layers:
+            if type(layer) is Layer:
+                layer.move_scanner()
 
 
 class Layer:
     def __init__(self, range):
-        self.list = [False] * range
-        self.list[0] = True
+        self.list = [0] * range
+        self.list[0] = 1
         self.pos = 0
 
         self.moving_down = True
 
-    def move(self):
+    def move_scanner(self):
+        self.list[self.pos] = 0
+
         if self.moving_down:
             self.pos += 1
         else:
             self.pos -= 1
 
-        if self.pos in (0, self.range - 1):
+        self.list[self.pos] = 1
+
+        if self.pos in (0, len(self.list) - 1):
             self.moving_down = not self.moving_down
+
+    def __repr__(self):
+        return f'Layer({len(self.list)})'
 
 
 def get_input():
@@ -34,7 +67,10 @@ def get_input():
         return [(int(r(l).group(1)), int(r(l).group(2))) for l in f]
 
 def main():
-    print(get_input())
+    f = Firewall(get_input())
+    # f = Firewall([(0, 3), (1, 2), (4, 4), (6, 4)])
+    f.move()
+    print(f.penalty)
 
 
 if __name__ == '__main__':
