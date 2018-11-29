30370e0508d7b8756c77058302a2df7d3a38d067
Foxscotch <foxscotch@gmail.com>
2018-11-29 09:29:29 -0600

    Progress on 2016.04.1
    It's not really working right, but I want to commit at this point for
    posterity. It returns 254, which the website says is too low.

--- 2016/04/p1.js
+++ 2016/04/p1.js
@@ -5,13 +5,42 @@ fs = require('fs');
 
 const input = fs.readFileSync('input.txt', 'utf-8').trim().split('\n');
 
-const start = process.hrtime.bigint();
 
+function getChecksum(name) {
+  let ltrObj = {};
 
-for (let room of input) {
-  let [_, name, id, checksum] = room.match(/([\w-]+)-(\d{3})\[(\w+)\]/);
-  name = name.replace(/-/g, '');
+  for (let char of name.replace(/-/g, '')) {
+    if (typeof ltrObj[char] === 'undefined')
+      ltrObj[char] = 1;
+    else
+      ltrObj[char]++;
+  }
+
+  let ltrList = [];
+
+  for (let [ltr, qty] of Object.entries(ltrObj)) {
+    ltrList.push({ ltr, qty });
+  }
+
+  return ltrList.sort((a, b) => {
+    if (a.qty === b.qty) {
+      if (a.ltr > b.ltr)
+        return 1;
+      else if (a.ltr < b.ltr)
+        return -1;
+    } else {
+      return b.qty - a.qty;
+    }
+  }).map(e => e.ltr).join('').slice(0, 5);
 }
 
 
-console.log(process.hrtime.bigint() - start);
+count = 0;
+
+for (let room of input) {
+  let [_, name, id, checksum] = room.match(/([\w-]+)-(\d{3})\[(\w+)\]/);
+  if (checksum === getChecksum(name))
+    count++;
+}
+
+console.log(count);
