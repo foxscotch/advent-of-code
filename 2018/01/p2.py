bcbd41b541804d7c45d484d7cc59c8bc7d96b0c3
Foxscotch <foxscotch@gmail.com>
2018-12-01 18:42:40 -0500

    Complete 2018.01

---
+++ 2018/01/p2.py
@@ -0,0 +1,22 @@
+# Python 3.6.1
+
+from collections import deque
+from itertools import cycle
+
+
+with open('input.txt', 'r') as f:
+    puzzle_input = [int(i) for i in f.read().split()]
+
+
+def main():
+    frequencies = set()
+    freq = 0
+
+    for i in cycle(puzzle_input):
+        if freq in frequencies:
+            return freq
+        frequencies.add(freq)
+        freq += i
+
+
+print(main())
