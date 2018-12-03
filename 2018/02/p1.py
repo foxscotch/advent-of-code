d0847e1394c5054f2e2885fd0e503dd6e338e8ac
Foxscotch <foxscotch@gmail.com>
2018-12-03 17:08:17 -0500

    Complete 2018.02

---
+++ 2018/02/p1.py
@@ -0,0 +1,25 @@
+# Python 3.6.1
+
+from collections import defaultdict
+
+
+with open('input.txt', 'r') as f:
+    puzzle_input = f.read().split()
+
+two_of_same_letter = 0
+three_of_same_letter = 0
+
+def appearances(id):
+    counts = defaultdict(lambda: 0)
+    for char in id:
+        counts[char] += 1
+    return 2 in counts.values(), 3 in counts.values()
+
+for id in puzzle_input:
+    twos, threes = appearances(id)
+    if twos:
+        two_of_same_letter += 1
+    if threes:
+        three_of_same_letter += 1
+
+print(two_of_same_letter * three_of_same_letter)
---
+++ 2018/02/p2.py
@@ -0,0 +1,31 @@
+# Python 3.6.1
+
+from collections import defaultdict
+
+
+with open('input.txt', 'r') as f:
+    puzzle_input = f.read().split()
+
+two_of_same_letter = 0
+three_of_same_letter = 0
+
+def difference(id1, id2):
+    count = 0
+    for a, b in zip(id1, id2):
+        if a != b:
+            count += 1
+    return count
+
+def main():
+    for id1 in puzzle_input:
+        for id2 in puzzle_input:
+            if difference(id1, id2) == 1:
+                return id1, id2
+
+
+answer = ''
+for a, b in zip(*main()):
+    if a == b:
+        answer += a
+
+print(answer)
