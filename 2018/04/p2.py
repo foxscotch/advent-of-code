bba3167377da9c6fd3ce1ff2ef654271ae4cede1
Foxscotch <foxscotch@gmail.com>
2018-12-06 15:22:23 -0600

    Complete 2018.04.2
    Still ugly tho.

---
+++ 2018/04/p2.py
@@ -0,0 +1,92 @@
+# Python 3.6.1
+
+import re
+from collections import defaultdict, namedtuple
+from datetime import datetime, timedelta
+
+
+Record = namedtuple('Record', ['date', 'guard', 'event'])
+
+
+def minutes(start, end):
+    date = start
+    while True:
+        yield date.minute
+        date = date + timedelta(minutes=1)
+        if date == end:
+            break
+
+def get_input():
+    input_r = re.compile(r'\[(.+)\] (.+)')
+    guard_r = re.compile(r'.+#(\d+)')
+    date_format = '%Y-%m-%d %H:%M'
+
+    records = []
+
+    with open('input.txt', 'r') as f:
+        guard = None
+        for line in f:
+            match = input_r.match(line)
+            record = [datetime.strptime(match[1], date_format), None, None]
+            if '#' in match[2]:
+                record[1] = int(guard_r.match(match[2])[1])
+                record[2] = 'begin'
+            elif match[2] == 'falls asleep':
+                record[2] = 'sleep'
+            else:
+                record[2] = 'wake'
+            records.append(record)
+        records.sort(key=lambda r: r[0])
+
+    guard = None
+    real_records = []
+    for record in records:
+        if record[1] is not None:
+            guard = record[1]
+        real_records.append(Record(record[0], guard, record[2]))
+    return real_records
+
+def main():
+    input = get_input()
+
+    # Guard objects:
+    #   <guard id>: {
+    #       asleep: <minutes spent sleeping>,
+    #       minutes: [
+    #           <times asleep during 0th minute>,
+    #           <times asleep during 1st minute>,
+    #           <times asleep during 2nd minute>,
+    #           ...
+    #           <times asleep during 59th minute>
+    #       ]
+    #   }
+
+    guards = defaultdict(lambda: { 'asleep': 0, 'minutes': [0] * 60 })
+    sleep_time = None
+    for record in input:
+        if record.event == 'sleep':
+            sleep_time = record.date
+        elif record.event == 'wake':
+            guards[record.guard]['asleep'] += (record.date - sleep_time).seconds // 60
+            for minute in minutes(sleep_time, record.date):
+                guards[record.guard]['minutes'][minute] += 1
+    
+    new_guards = defaultdict(lambda: 0)
+    for guard, data in guards.items():
+        most_common_minute = None
+        minute_value = 0
+        for i, times in enumerate(data['minutes']):
+            if times > minute_value:
+                most_common_minute = i
+                minute_value = times
+        new_guards[guard] = most_common_minute, minute_value
+    guard, (minute, val) = max(new_guards.items(), key=lambda g: g[1][1])
+    print(guard * minute)
+
+
+if __name__ == '__main__':
+    import time
+
+    start = time.perf_counter()
+    main()
+    print(time.perf_counter() - start)
