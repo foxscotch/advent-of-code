2e4a1a3b56dea0308281ffd9d1751d7ed78abab3
Foxscotch <foxscotch@gmail.com>
2018-12-10 14:17:49 -0600

    Complete 2018.07.1
    Yeah, it's a little overengineered. But it's ok.

--- 2018/07/p1.py
+++ 2018/07/p1.py
@@ -3,6 +3,7 @@
 import re
 import string
 from functools import total_ordering
+from itertools import cycle
 
 
 @total_ordering
@@ -10,12 +11,14 @@ class Step:
     def __init__(self, name):
         self.name = name
         self.requires = []
-        self.required_by = []
 
     def require(self, step):
         if step not in self.requires:
             self.requires.append(step)
-            step.required_by.append(self)
+
+    def satisfy(self, step):
+        if step in self.requires:
+            self.requires.remove(step)
 
     def __repr__(self):
         return f"Step({self.name})"
@@ -30,6 +33,24 @@ class Step:
 class StepCollection(dict):
     def __init__(self):
         super().__init__({l: Step(l) for l in string.ascii_uppercase})
+        self.results = []
+
+    def run(self):
+        while True:
+            for step in self.values():
+                if step in self.results:
+                    continue
+
+                if len(step.requires) == 0:
+                    self.satisfy_all(step)
+                    self.results.append(step)
+                    if len(self.results) >= 26:
+                        return ''.join([s.name for s in self.results])
+                    break
+
+    def satisfy_all(self, required):
+        for step in self.values():
+            step.satisfy(required)
 
     def __repr__(self):
         return '\n'.join([f"{x}: {x.requires}" for x in sorted(self.values())])
@@ -48,7 +69,7 @@ def main():
     steps = StepCollection()
     for step, requires in get_input():
         steps.get(step).require(steps.get(requires))
-    print(steps)
+    print(steps.run())
 
 
 if __name__ == '__main__':
