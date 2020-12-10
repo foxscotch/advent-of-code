547842582b642b9053347faaa78fafa2b04732ac
Foxscotch <foxscotch@gmail.com>
2020-12-10 09:50:20 -0600

    Complete 2020.10.2
    This one I was unable to do alone. I got some hints that it could be
    solved as a tribonacci sequence (which is like fibonacci but... three),
    and that "Dynamic Programming" could help, and this is where I ended up.
    I guess that's good though cus it means I learned something.

---
+++ 2020/10/p2.py
@@ -0,0 +1,34 @@
+# Python 3.8.3
+
+from collections import defaultdict
+
+
+def count_paths(chain):
+    d = defaultdict(int)
+    d[0] = 1
+    for i in chain:
+        d[i] = d[i - 3] + d[i - 2] + d[i - 1]
+    return d[i]
+
+def get_input():
+    with open('input.txt', 'r') as f:
+        return set(int(i) for i in f.read().split())
+
+def main():
+    puzzle = get_input()
+
+    d = defaultdict(int)
+    d[0] = 1
+
+    for i in puzzle:
+        d[i] = d[i - 3] + d[i - 2] + d[i - 1]
+
+    return d[i]
+
+
+if __name__ == '__main__':
+    import time
+
+    start = time.perf_counter()
+    print(main())
+    print(time.perf_counter() - start)
