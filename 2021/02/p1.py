7b0d523393b31fbd725312fd34cbd895929e147d
foxscotch <foxscotch@gmail.com>
2021-12-02 22:47:54 -0600

    Complete 2021.02

---
+++ 2021/02/p1.py
@@ -0,0 +1,51 @@
+# Python 3.10.0
+from enum import Enum
+from typing import NamedTuple
+
+
+class Dir(Enum):
+    F = 1
+    D = 2
+    U = 3
+
+
+class Instruction(NamedTuple):
+    direction: Dir
+    distance: int
+
+
+class Position(NamedTuple):
+    distance: int
+    depth: int
+
+
+def get_input():
+    with open("input.txt", "r") as f:
+        for dir, dist in [l.split() for l in f]:
+            yield Instruction(Dir[dir.upper()[0]], int(dist))
+
+
+def main():
+    puzzle = get_input()
+
+    position = Position(0, 0)
+
+    for instr in puzzle:
+        match instr.direction:
+            case Dir.F:
+                position = Position(position.distance + instr.distance, position.depth)
+            case Dir.D:
+                position = Position(position.distance, position.depth + instr.distance)
+            case Dir.U:
+                position = Position(position.distance, position.depth - instr.distance)
+
+    return position.distance * position.depth
+
+
+
+if __name__ == "__main__":
+    import time
+
+    start = time.perf_counter()
+    print(main())
+    print(time.perf_counter() - start)
